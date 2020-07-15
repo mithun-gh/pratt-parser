59dbf486163abd8a6b562340f35359ffecb6aad1
Mithun <mithunrathinasamy@live.com>
2020-07-15 23:21:39 +1000

    add eval

--- src/interpreter/eval.rs
+++ src/interpreter/eval.rs
@@ -1,10 +1,26 @@
-use crate::interpreter::parser::Expression;
+use crate::interpreter::parser::{Expression, Operator};
+
+impl Operator {
+    fn eval(&self, left: f64, right: f64) -> f64 {
+        match self {
+            Operator::Add => left + right,
+            Operator::Sub => left - right,
+            Operator::Mul => left * right,
+            Operator::Div => left / right,
+        }
+    }
+}
 
 #[derive(Debug, Copy, Clone)]
 pub enum ArithmaticError {
     DivideByZero,
 }
 
-pub fn evaluate(_expr: Expression) -> Result<f64, ArithmaticError> {
-    Ok(0.0)
+pub fn evaluate(expr: Expression) -> Result<f64, ArithmaticError> {
+    match expr {
+        Expression::Number(n) => Ok(n),
+        Expression::Binary(left, op, right) => {
+            Ok(op.eval(evaluate(*left)?, evaluate(*right)?))
+        }
+    }
 }
